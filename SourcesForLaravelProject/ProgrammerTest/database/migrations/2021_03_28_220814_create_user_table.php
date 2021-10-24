3c79733063fd7ef35e9de96d2a5f8f07f7865e82
MarcAnthony-Palmer <69550799+MarcAnthony-Palmer@users.noreply.github.com>
2021-10-24 13:06:01 -0500

    Updating Migrations

--- SourcesForLaravelProject/ProgrammerTest/database/migrations/2021_03_28_220814_create_user_table.php
+++ SourcesForLaravelProject/ProgrammerTest/database/migrations/2021_03_28_220814_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateAdminTable extends Migration
      */
     public function up()
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
-            $table -> string('email');
-            $table -> string('password');
         });
     }
 
@@ -28,6 +31,6 @@ class CreateAdminTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin');
+        Schema::dropIfExists('users');
     }
 }
--- SourcesForLaravelProject/ProgrammerTest/database/seeders/DatabaseSeeder.php
+++ SourcesForLaravelProject/ProgrammerTest/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
              CompanySeeder::class,
              EmployeeSeeder::class,
+             UserSeeder::class
         ]);
     }
 }
